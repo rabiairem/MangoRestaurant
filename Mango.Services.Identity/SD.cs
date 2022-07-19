0b5f3b20f3375eb14a80263d9236f1c97bc6d084
Rabia Irem Ucar <rabia-irem.ucar@capgemini.com>
2022-07-19 15:31:03 +0200

    login page couldnt open because of the wrong port number in SD(idendity project) and add registeration code a bit

--- Mango.Services.Identity/SD.cs
+++ Mango.Services.Identity/SD.cs
@@ -40,8 +40,8 @@ namespace Mango.Services.Identity
                 ClientId = "mango",
                 ClientSecrets = { new Secret("secret".Sha256()) },
                 AllowedGrantTypes = GrantTypes.Code,
-                RedirectUris = { "https://localhost:44330/signin-oidc" },
-                PostLogoutRedirectUris = { "https://localhost:44330/signout-callback-oidc" },
+                RedirectUris = { "https://localhost:7092/signin-oidc" },
+                PostLogoutRedirectUris = { "https://localhost:7092/signout-callback-oidc" },
                 AllowedScopes = new List<string>
                 {
                     IdentityServerConstants.StandardScopes.OpenId,
